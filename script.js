c6d1bd850c191ac7170e860734cdda322c57920a
newcomer119 <mitarth.125035@stu.upes.ac.in>
2024-11-04 00:05:57 +0530

    changed things

--- script.js
+++ script.js
@@ -7,13 +7,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-window.addEventListener('scroll', function() {
-    const gallerySection = document.getElementById('gallery');
-    const rect = gallerySection.getBoundingClientRect();
-    
-    // Check if the gallery section is in the viewport
-    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-        gallerySection.style.opacity = 1; // Make it visible
-        gallerySection.classList.add('fade-in'); // Add class for animation
-    }
-});
